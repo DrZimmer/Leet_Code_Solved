683cdf063ab81e8dec9bc84c9560527b1d9f459f
White Tiger <dayley.zimmermann@gmail.com>
2022-11-08 21:50:06 -0500

    breadth first search, basically the same except using shift / push instead of pop/push

--- Algorithms/Graph_Algorithms/practice.js
+++ Algorithms/Graph_Algorithms/practice.js
@@ -15,15 +15,26 @@
 // };
 
 //recursive
-const depthFirstPrint = (graph, source) => {
-  console.log(source);
-  for (let neighbor of graph[source]) {
-    depthFirstPrint(graph, neighbor);
+// const depthFirstPrint = (graph, source) => {
+//   console.log(source);
+//   for (let neighbor of graph[source]) {
+//     depthFirstPrint(graph, neighbor);
+//   }
+// };
+
+const breadthFirstPrint = (graph, source) => {
+  const queue = [source];
+  while (queue.length > 0) {
+    const current = queue.shift();
+    console.log(current);
+    for (let neighbor of graph[current]) {
+      queue.push(neighbor);
+    }
   }
 };
 
 const graph = {
-  a: ["b", "c"],
+  a: ["c", "b"],
   b: ["d"],
   c: ["e"],
   d: ["f"],
@@ -31,5 +42,6 @@ const graph = {
   f: [],
 };
 //depth first traversal
-depthFirstPrint(graph, "a");
+// depthFirstPrint(graph, "a");
 //breadth first traversal
+breadthFirstPrint(graph, "a");
