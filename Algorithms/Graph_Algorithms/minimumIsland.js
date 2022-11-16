291dc5e63180ce91103ded71c7cde709722d4c0a
White Tiger <dayley.zimmermann@gmail.com>
2022-11-16 14:02:38 -0500

    minimum island

---
+++ Algorithms/Graph_Algorithms/minimumIsland.js
@@ -0,0 +1,35 @@
+const minimumIsland = (grid) => {
+  const visited = new Set();
+
+  let minSize = Infinity;
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[0].length; c++) {
+      const size = exploreSize(grid, r, c, visited);
+      if (size > 0 && size < minSize) {
+        minsize = size;
+      }
+    }
+  }
+
+  return minSize;
+};
+
+const exploreSize = (grid, r, c, vsited) => {
+  const rowInbounds = 0 <= r && r < grid.length;
+  const colInbounds = 0 <= c && c < grid[0].length;
+  if (!rowInbounds || !colInbounds) return 0;
+
+  if (grid[r][c] === "W") return 0;
+
+  const pos = r + "," + c;
+  if (visited.has(pos)) return 0;
+  visited.add(pos);
+
+  let size = 1;
+  size += explore(grid, r - 1, c, visited);
+  size += explore(grid, r + 1, c, visited);
+  size += explore(grid, r, c - 1, visited);
+  size += explore(grid, r, c + 1, visited);
+
+  return size;
+};
